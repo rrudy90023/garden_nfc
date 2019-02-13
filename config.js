edb13bc98821822efc47ee3c56e736608081b56d
RUDY PEREZ <rudyperez@RUDYs-MacBook-Air.local>
2019-02-13 15:29:03 -0800

    changed route

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
-//config.mongoUri = 'mongodb://rrudy90023:sanctuary3437@ds237574.mlab.com:37574/sanctuary3437';
-config.mongoUri = 'mongodb://localhost:27017/gardennfc';
+config.mongoUri = 'mongodb://rrudy90023:sanctuary3437@ds237574.mlab.com:37574/sanctuary3437';
+//config.mongoUri = 'mongodb://localhost:27017/gardennfc';
 config.cookieMaxAge = 30 * 24 * 3600 * 1000;
 
 module.exports = config;
