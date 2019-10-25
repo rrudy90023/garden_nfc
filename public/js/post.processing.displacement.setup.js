56ac514f889889d65509d02f9917fb82d541c9d3
RUDY PEREZ <rudyperez@RUDYs-MacBook-Air.local>
2019-10-25 16:19:59 -0700

    added images folder

--- public/js/post.processing.displacement.setup.js
+++ public/js/post.processing.displacement.setup.js
@@ -149,9 +149,9 @@ window.addEventListener("load", function() {
     var shaderPass = webGLCurtain.addShaderPass(shaderPassParams);
     // we will need to load a new image
     var image = new Image();
-    // image.src = "../images/stucco_4_1024.jpg";
+
     image.crossOrigin = "anonymous";
-    image.src = "https://a2plcpnl0011.prod.iad2.secureserver.net:2083/cpsess0189925883/viewer/home%2fuihx2xdnoigi%2fpublic_html%2fimages/stucco_4_1024.jpg"
+    image.src = "../images/stucco_4_1024.jpg"
     
     // set its data-sampler attribute to use in fragment shader
     image.setAttribute("data-sampler", "displacementTexture");
