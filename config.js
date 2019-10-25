8754dffcdf79bda2d53df72bf55c97e638e84dd7
RUDY PEREZ <rudyperez@RUDYs-MacBook-Air.local>
2019-10-25 16:20:22 -0700

    added images folder 2

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {};
 
-//config.mongoUri = 'mongodb://rrudy90023:sanctuary3437@ds237574.mlab.com:37574/sanctuary3437';
-config.mongoUri = 'mongodb://localhost:27017/gardennfc';
+config.mongoUri = 'mongodb://rrudy90023:sanctuary3437@ds237574.mlab.com:37574/sanctuary3437';
+//config.mongoUri = 'mongodb://localhost:27017/gardennfc';
 config.cookieMaxAge = 30 * 24 * 3600 * 1000;
 //config.corsURL = 'https://rudes.de/s17'
 module.exports = config;
