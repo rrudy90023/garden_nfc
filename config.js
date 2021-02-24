dd14864e78d61a6fbadfd1c0b0e28bd7be2345d5
RUDY PEREZ <rudyperez@RUDYs-MacBook-Air.local>
2021-02-24 11:35:51 -0800

    migrate to atlas

--- config.js
+++ config.js
@@ -1,7 +1,14 @@
 const config = {};
 
-config.mongoUri = 'mongodb://rrudy90023:sanctuary3437@ds237574.mlab.com:37574/sanctuary3437';
+//mLab
+//config.mongoUri = 'mongodb://rrudy90023:sanctuary3437@ds237574.mlab.com:37574/sanctuary3437';
+
+//Atlas
+config.mongoUri = 'mongodb+srv://rrudy90023:g8@00%Az@s17.3sgsp.mongodb.net/sanctuary3437?retryWrites=true&w=majority';
+
+//Local
 //config.mongoUri = 'mongodb://localhost:27017/gardennfc';
+
 config.cookieMaxAge = 30 * 24 * 3600 * 1000;
 //config.corsURL = 'https://rudes.de/s17'
 module.exports = config;
