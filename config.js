fc561bbb6f4ebbeb17f10e053e80c5196baeb779
RUDY PEREZ <rudyperez@RUDYs-MacBook-Air.local>
2021-02-24 16:38:08 -0800

    change creds

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const config = {};
 //config.mongoUri = 'mongodb://rrudy90023:sanctuary3437@ds237574.mlab.com:37574/sanctuary3437';
 
 //Atlas
-config.mongoUri = 'mongodb+srv://rrudy90023:FOhLChxMoAn3kpNq@s17.3sgsp.mongodb.net/s17?retryWrites=true&w=majority';
+config.mongoUri = 'mongodb+srv://rrudy90023:FOhLChxMoAn3kpNq@s17.3sgsp.mongodb.net/sanctuary3437?retryWrites=true&w=majority';
 
 //Local
 //config.mongoUri = 'mongodb://localhost:27017/gardennfc';
